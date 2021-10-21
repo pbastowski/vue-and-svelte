1d401dd724d301066702c4f62dd6150026d5b033
Paul Bastowski <pbastowski@gmail.com>
2021-10-21 16:02:38 +0200

    Refactor to use script-setup and template syntax, which is much less verbose (less code)

--- src/components/VInput.vue
+++ src/components/VInput.vue
@@ -1,10 +1,28 @@
+<template>
+    <slot name="previous" :dec="dec" />
+    <slot v-bind="{ abc, dec, inc }" />
+    <slot name="next" :inc="inc" />
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+
+    const abc = ref(1)
+    const dec = () => abc.value--
+    const inc = () => abc.value++
+
+    console.log('VINPUT')
+</script>
+
+<!--
 <script>
+    /* And this is the nos script-setup version that returns a render function */
     import { ref } from 'vue'
 
     export default {
         setup(props, ctx) {
             const abc = ref(1)
-            const dec = () => abc.value--
+            const dec = () => abc.value&#45;&#45;
             const inc = () => abc.value++
 
             console.log('CTX:', ctx.slots)
@@ -21,3 +39,4 @@
     }
     console.log('VINPUT')
 </script>
+-->
